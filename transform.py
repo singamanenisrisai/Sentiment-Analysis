aa6ec3de1fac5ba0192289c7e72436f62917aaab
hemanthkurra <35706378+hemanthkurra@users.noreply.github.com>
2018-06-07 18:05:01 -0300

    updated files

--- transform.py
+++ transform.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 import pandas as pnd
 dataoutput = pnd.read_csv("tweets.csv")
-print(dataoutput)
+SentiFinal = pnd.DataFrame()
 
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag,pos_tag_sents
 
 tweets = dataoutput["text"].map(lambda x: x.lower())
-print(tweets)
 
 dataoutput["tweetinput"]=tweets
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -18,21 +17,40 @@ negative=[]
 positive=[]
 neutral=[]
 compound=[]
+sentiAnalysis=[]
+sentiScore = []
 
 for tweet in tweets:
     sentiValues = output.polarity_scores(tweet)
-    # print(sentiValues["neg"])
     negative.append(sentiValues["neg"])
     positive.append(sentiValues["pos"])
     neutral.append(sentiValues["neu"])
     compound.append(sentiValues["compound"])
-    
-    # print("{:-<65} {}".format(tweet, str(sentiValues)))
+
+    if sentiValues["neg"]>sentiValues["pos"]:
+        sentiAnalysis.append("Negative")
+        sentiScore.append(sentiValues["neg"])
+    elif sentiValues["pos"]>sentiValues["neg"]:
+        sentiAnalysis.append("Positive")
+        sentiScore.append(sentiValues["pos"])
+    else:
+        sentiAnalysis.append("Neutral")
+        sentiScore.append(sentiValues["neu"])
+
 dataoutput["positive"] = positive
 dataoutput["neutral"] = neutral
 dataoutput["negative"] = negative
 dataoutput["compound"] = compound
+dataoutput["SentiAnalysis"] = sentiAnalysis
+dataoutput["SentiScore"] = sentiScore
 
+SentiFinal["Tweets"] = dataoutput["tweetinput"]
+SentiFinal["SentimentAnalysis"] = dataoutput["SentiAnalysis"]
+SentiFinal["SentimentScore"] = dataoutput["SentiScore"]
+
+print(SentiFinal)
 print(dataoutput)
 
-dataoutput.to_csv("sentiTweets.csv")
+SentiFinal.to_csv("FinalSentiment.csv", index=False)
+dataoutput.to_csv("sentiTweets.csv", index=False)
+
